1a06c07a0f553ffdb933f7b816e29826e80b985f
slawekj <janusz.slawek@gmail.com>
2013-10-02 11:52:22 -0400

    add license statement and check names

--- ennet/R/v2l.R
+++ ennet/R/v2l.R
@@ -1,5 +1,22 @@
-v2l = function(V,max=100000) {
-  V            = data.frame(V)
+#    This is an implementation of ENNET algorithm for Gene Regulatory Network
+#    inference from mRNA expression data, in form of an R package.
+#    Copyright (C) 2013  Janusz Slawek
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program, see LICENSE.
+
+v2l = function(V,max=100000,check.names=TRUE) {
+  V            = data.frame(V,check.names=check.names)
   N            = nrow(V)
   M            = ncol(V)
   targets      = colnames(V)
